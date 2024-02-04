ecbe832531ed9673cb2fdd24c1d13df7b572727c
Dmitrii Vasilev <148041419+dv026-fa@users.noreply.github.com>
2024-02-04 14:05:24 -0300

    remove old stuff

--- src/models/game-model.ts
+++
@@ -1,4 +0,0 @@
-export interface GameModel {
-  name: string
-  acoountId: string
-}
--- src/models/request-model.ts
+++
@@ -1,9 +0,0 @@
-import { GameModel } from './game-model'
-import { UserModel } from './user-model'
-
-export interface RequestModel {
-  game: GameModel
-  // currency: string;
-  amount: string
-  user: UserModel
-}
